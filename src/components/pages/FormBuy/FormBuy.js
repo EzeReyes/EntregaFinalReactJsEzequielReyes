2cc236f0fa03ce21cf6f3e73b9f18ed2fc2d2afd
ezequiel reyes <110490540+EzeReyes@users.noreply.github.com>
2023-03-14 12:27:15 -0300

    Agreg Forms

--- src/components/pages/FormBuy/FormBuy.js
+++ src/components/pages/FormBuy/FormBuy.js
@@ -1,30 +1,81 @@
-import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from "react";
 
+// Firebase
+import { db } from "../../../Firebase/Firebase";
+import { collection, addDoc } from "firebase/firestore";
+
+import MessageSuccess from "../../MessageSuccess/MessageSuccess";
+
+import "./FormBuy.css";
+
+import TextField from "@mui/material/TextField";
+
+const styles = {
+  containerShop: {
+    textAlign: "center",
+    paddingTop: 20,
+  },
+};
+
+const initialState = {
+  name: "",
+  lastName: "",
+  city: "",
+};
 
 const FormBuy = () => {
+  const [values, setValues] = useState(initialState);
+  const [purchaseID, setPurchaseID] = useState("");
 
-return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Nombre: </Form.Label>
-        <Form.Control type="name" placeholder="Ingrese su nombre" />
-        <Form.Text className="text-muted">
-        </Form.Text>
-      </Form.Group>
+  // console.log(purchaseID);
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Apellido: </Form.Label>
-        <Form.Control type="subname" placeholder="Ingrese su Apellido" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Enviar
-      </Button>
-    </Form>
+  const handleOnChange = (e) => {
+    const { value, name } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    // Add a new document with a generated id.
+    const docRef = await addDoc(collection(db, "purchases"), {
+      values,
+    });
+    // console.log("Document written with ID: ", docRef.id);
+    setPurchaseID(docRef.id);
+    setValues(initialState);
+  };
+
+  return (
+    <div style={styles.containerShop}>
+      <h1>Finalizar Compra</h1>
+      <form className="FormContainer" onSubmit={handleOnSubmit}>
+        <TextField
+          placeholder="Name"
+          style={{ margin: 10, width: 400 }}
+          name="name"
+          value={values.name}
+          onChange={handleOnChange}
+        />
+        <TextField
+          placeholder="Last Name"
+          style={{ margin: 10, width: 400 }}
+          name="lastName"
+          value={values.lastName}
+          onChange={handleOnChange}
+        />
+        <TextField
+          placeholder="email"
+          style={{ margin: 10, width: 400 }}
+          name="email"
+          value={values.city}
+          onChange={handleOnChange}
+        />
+        <button className="btnASendAction">Send</button>
+      </form>
+      {purchaseID ? <MessageSuccess purchaseID={purchaseID} /> : null}
+    </div>
   );
-}
-
+};
 
 
 export default FormBuy;
